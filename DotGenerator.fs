e6133be04f13bb3d0cd04f5b47213b7c2961be73
zoren <ss@configit.com>
2016-03-08 00:34:09 +0100

    initial commit

---
+++ DotGenerator.fs
@@ -0,0 +1,67 @@
+﻿namespace HoneyComb
+
+module GraphViz =
+    // change this as needed for your local environment
+    let graphVizPath = @"C:\Program Files (x86)\Graphviz2.34\bin\"
+
+    let writeEdge writer (fromNode, label, toNode) =
+        fprintfn writer " %s -> %s[label = %s];" fromNode toNode label
+
+    // Create a DOT file for graphviz to read.
+    let createDotFile dotFilename edges nodes =
+        let inFile = System.IO.Path.Combine(__SOURCE_DIRECTORY__,dotFilename)
+        use writer = new System.IO.StreamWriter(path=inFile)
+        fprintfn writer "digraph G {"
+        fprintfn writer "    fontsize=10;"
+        nodes
+        |> Seq.iter (fun(name, label) -> fprintfn writer "\"%s\" [label=\"%s\"];" name label)
+        edges
+        |> Seq.iter (writeEdge writer)
+        fprintfn writer "   }"
+
+    // shell out to run a command line program
+    let startProcessAndCaptureOutput cmd cmdParams =
+        let debug = false
+
+        if debug then
+            printfn "Process: %s %s" cmd cmdParams
+        let si = new System.Diagnostics.ProcessStartInfo(cmd, cmdParams)
+        si.UseShellExecute <- false
+        si.RedirectStandardOutput <- true
+        use p = new System.Diagnostics.Process()
+        p.StartInfo <- si
+        if p.Start() then
+            if debug then
+                use stdOut = p.StandardOutput
+                stdOut.ReadToEnd() |> printfn "%s"
+                printfn "Process complete"
+        else
+            failwith "Process failed"
+
+    /// Generate an image file from a DOT file
+    /// algo = dot, neato
+    /// format = gif, png, jpg, svg
+    let generateImageFile dotFilename layout algo format imgFilename =
+        let cmd = sprintf @"""%s%s.exe""" graphVizPath algo
+        let inFile = System.IO.Path.Combine(__SOURCE_DIRECTORY__,dotFilename)
+        let outFile = System.IO.Path.Combine(__SOURCE_DIRECTORY__,imgFilename)
+        let cmdParams = sprintf "-K%s -T%s -o\"%s\" \"%s\"" layout format outFile inFile
+        startProcessAndCaptureOutput cmd cmdParams
+
+module DotGenerator =
+  open ShortestPath
+
+  let cellToDotEdges (cell:Cell<'a>) =
+    cell.Neighbours |> Seq.mapi (fun i toNodeOpt -> toNodeOpt |> Option.map (fun toNode -> cell, i, toNode)) |> Seq.choose id
+
+  let cellsToDot cells =
+    let n2s (c:Cell<int>) = string c.Label
+    let edges = Seq.collect cellToDotEdges cells |> Seq.map (fun (f, i, t) -> n2s f, i.ToString(), n2s t)
+    let nodes = cells |> Seq.map (fun cell -> n2s cell, n2s cell)
+    nodes, edges
+
+  let mkFile cells =
+    let nodes, edges = cellsToDot cells
+    let path = "tmp.dot"
+    GraphViz.createDotFile path edges nodes
+    GraphViz.generateImageFile path "neato" "dot" "png" "honey.png"
---
+++ ShortestPath.fs
@@ -0,0 +1,62 @@
+﻿namespace HoneyComb
+
+module ShortestPath =
+  let inc v i = (v + i + 6) % 6
+
+  type Cell<'a> =
+    {
+      Label : 'a
+      mutable Neighbours : Cell<'a> option[]
+    }
+
+  let mkCell label = {Label = label; Neighbours = Array.zeroCreate 6}
+
+  let connect c1 dir c2 =
+    let setNeighbour c dir c' =
+      if Option.isSome c.Neighbours.[dir]
+      then
+        failwithf "already set %A %i, was %A, trying to set %A" c1.Label dir (Option.get c.Neighbours.[dir]).Label <| c'.Label
+      c.Neighbours.[dir] <- Some c'
+    setNeighbour c1 dir c2
+    setNeighbour c2 (inc dir 3) c1
+
+  let addRing initCell ring =
+    let currentCellRef = ref initCell
+    let create dir =
+      let currentCell = !currentCellRef
+      let newNode = mkCell <| currentCell.Label + 1
+      connect currentCell dir newNode
+      let rotDir = inc dir 1
+      let prevRotOpt = currentCell.Neighbours.[rotDir]
+      prevRotOpt
+        |> Option.iter (fun prevRot ->
+          connect newNode (inc dir 2) prevRot
+          prevRot.Neighbours.[dir]
+            |> Option.iter (fun n' -> connect newNode (inc dir 1) n')
+          )
+      currentCellRef := newNode
+    create 0
+    let startCell = !currentCellRef
+    for i = 0 to ring - 2 do
+      create 1
+    for dir = 2 to 6 do
+      for i = 0 to ring - 1 do
+        create <| dir%6
+    !currentCellRef
+
+  let findAll cell =
+    let s = System.Collections.Generic.HashSet(HashIdentity.Reference)
+    let rec loop c =
+      if s.Add c
+      then c.Neighbours |> Seq.choose id |> Seq.iter loop
+      else ()
+    loop cell
+    s :> _ seq
+
+  let mkComb n =
+    assert (n >= 0)
+    let center = mkCell 1
+    let c = ref center
+    for i = 1 to n do
+      c := addRing !c i
+    findAll !c
